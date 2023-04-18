c975fc9a6d240921e0089d598dbcb5c43e8ef63c
若海 <wang@rehiy.com>
2023-04-18 22:33:42 +0800

    feat: 增加在线更新提示

--- src/api/native/index.ts
+++ src/api/native/index.ts
@@ -3,6 +3,7 @@ import { CacheModel } from "./cache"
 import { ArtworkModel } from "./artwork"
 import { ConfigModel } from "./config"
 import { PassportModel } from "./passport"
+import { UpgradeModel } from "./upgrade"
 import { UserModel } from "./user"
 
 export default {
@@ -11,5 +12,6 @@ export default {
     artwork: new ArtworkModel(),
     config: new ConfigModel(),
     passport: new PassportModel(),
+    upgrade: new UpgradeModel(),
     user: new UserModel(),
 }
--- src/apps/dashboard/index.vue
+++ src/apps/dashboard/index.vue
@@ -14,6 +14,7 @@ export default class DashboardIndex extends Vue {
 
     public created() {
         this.getUserSummary()
+        this.checkUpgrade()
     }
 
     // 资源统计
@@ -24,25 +25,57 @@ export default class DashboardIndex extends Vue {
         const res = await NaApi.passport.summary()
         this.summary = res
     }
+
+    // 在线更新
+
+    public newVersion = ""
+    public upgrading = false
+
+    async checkUpgrade() {
+        const res = await NaApi.upgrade.check()
+        if (res.BinaryUrl.startsWith("https://")) {
+            this.newVersion = res.Version
+        }
+    }
+
+    public applyUpgrade() {
+        this.upgrading = true
+        NaApi.upgrade.apply().finally(() => {
+            this.upgrading = false
+            location.reload()
+        })
+    }
 }
 </script>
 
 <template>
-    <t-card :loading="!summary" hover-shadow header-bordered>
-        <t-space fixed direction="vertical">
-            <p>欢迎使用智能绘画平台，画图能力基于腾讯云<b>Aiart</b>接口实现</p>
-            <t-space v-if="summary" fixed break-line>
-                <t-card v-route="'/artwork/list'" class="summary" hover-shadow>
-                    <div>图库</div>
-                    <b>{{ summary.Artwork }}</b>
-                </t-card>
-                <t-card class="summary" hover-shadow>
-                    <div>配额</div>
-                    <b>{{ summary.ArtworkQuota }}</b>
-                </t-card>
+    <t-space fixed direction="vertical">
+        <t-alert v-if="newVersion" theme="info" close>
+            <template #message>
+                检测到新版本 v{{ newVersion }}，在线更新完成后将自动刷新页面。
+            </template>
+            <template #operation>
+                <t-loading v-if="upgrading" size="12px" />
+                <span v-else @click="applyUpgrade">立即更新</span>
+            </template>
+        </t-alert>
+
+        <t-card :loading="!summary" hover-shadow header-bordered>
+            <t-space fixed direction="vertical">
+                <p>欢迎使用智能绘画平台，画图能力基于腾讯云<b>Aiart</b>接口实现</p>
+                <t-space v-if="summary" fixed break-line>
+                    <t-card v-route="'/artwork/list'" class="summary" hover-shadow>
+                        <div>图库</div>
+                        <b>{{ summary.Artwork }}</b>
+                    </t-card>
+                    <t-card class="summary" hover-shadow>
+                        <div>配额</div>
+                        <b>{{ summary.ArtworkQuota }}</b>
+                    </t-card>
+                </t-space>
             </t-space>
-        </t-space>
-    </t-card>
+        </t-card>
+    </t-space>
 </template>
 
 <style lang="scss" scoped>
