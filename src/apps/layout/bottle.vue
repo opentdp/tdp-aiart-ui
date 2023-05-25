77920984ff0e56bfbe65c2dc1a17b8ef030b5a02
若海 <wang@rehiy.com>
2023-05-25 08:52:14 +0800

    feat: 首页增加菜单

--- src/apps/layout/bottle.vue
+++ src/apps/layout/bottle.vue
@@ -16,23 +16,82 @@ export default class LayoutBottle extends Vue {
     public layout = layoutStore()
     public session = sessionStore()
 
+    // 菜单列表
+
+    public items: MenuItem[] = []
+
+    // 初始化
+
+    public created() {
+        this.items = this.itemFilter(menuItems)
+    }
+
+    public itemFilter(items: MenuItem[]) {
+        const level = this.session.Level
+        return items.filter(item => {
+            if (item.level && item.level < level) {
+                return false //用户组权限不足
+            }
+            if (item.subs) {
+                item.subs = this.itemFilter(item.subs)
+            }
+            return true
+        })
+    }
+
     // 切换主题模式
     public themeModeChange() {
         const mode = this.layout.ThemeMode == "dark" ? "light" : "dark"
         this.layout.setThemeMode(mode)
     }
 }
+
+interface MenuItem {
+    icon: string
+    index: string
+    title: string
+    level?: number
+    subs?: MenuItem[]
+}
+
+const menuItems: MenuItem[] = [
+    {
+        icon: "home",
+        index: "/",
+        title: "图库",
+    },
+    {
+        icon: "server",
+        index: "/artwork/create",
+        title: "文生图",
+    },
+    {
+        icon: "image",
+        index: "/artwork/create2",
+        title: "图生图",
+    },
+    {
+        icon: "precise-monitor",
+        index: "/chatbot/list",
+        title: "智能对话",
+    },
+]
 </script>
 
 <template>
     <t-layout>
         <t-layout>
             <t-header>
-                <t-head-menu>
+                <t-head-menu :value="$route.path">
                     <template #logo>
                         <a class="logo" href="/" :title="layout.SiteName">
                             <img :src="layout.Collapse ? layout.SiteIcon : layout.SiteLogo">
                         </a>
+                    </template>
+                    <template v-for="item in items" :key="item.index">
+                        <t-menu-item :value="item.index" :to="{ path: item.index }">
+                            {{ item.title }}
+                        </t-menu-item>
                     </template>
                     <template #operations>
                         <t-button theme="default" variant="text" @click="themeModeChange">
