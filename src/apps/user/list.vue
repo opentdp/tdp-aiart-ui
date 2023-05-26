59769b11e80f2b7039622bfc1a78a9aab6b12f1e
若海 <wang@rehiy.com>
2023-05-26 11:24:02 +0800

    sytle: 简化参数

--- src/apps/user/list.vue
+++ src/apps/user/list.vue
@@ -61,8 +61,7 @@ export default class UserList extends Vue {
             <template #subtitle>
                 记录总数: {{ userList?.length || 0 }}
             </template>
-            <t-table :loading="loading" :data="userList" :columns="tableColumns" :hover="true" row-key="Id"
-                cell-empty-content="--">
+            <t-table :loading="loading" :data="userList" :columns="tableColumns" row-key="Id" cell-empty-content="--" hover>
                 <template #Operation="{ row }">
                     <t-link theme="primary" hover="color" @click="updateModal.open(row)">
                         修改
